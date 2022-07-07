54d326c87445503ed63c566c9bb99528f24bc101
ben <ben@ajaxscape.com>
2022-07-08 00:27:51 +0100

    Add roles and users to database

--- src/components/fragments/Menu.js
+++ src/components/fragments/Menu.js
@@ -1,36 +1,62 @@
-import { ListItemText, MenuItem, MenuList } from '@mui/material'
+import { ListItemText, MenuItem, MenuList, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import React from 'react'
 import { useGlobal } from '../context/GlobalContext'
+import { styled } from '@mui/material/styles'
+import { useAuthentication } from '../context/AuthenticationContext'
+
+const PREFIX = 'LdgApp-Menu'
+
+const classes = {
+  title: `${PREFIX}-title`,
+}
+
+const Root = styled('div')(({ theme }) => ({
+  [`.${classes.title}`]: {
+    marginLeft: theme.spacing(2),
+    flexGrow: 1,
+  },
+}))
 
 export default function Menu() {
   const { menuVisible, setMenuVisible } = useGlobal()
+  const { user } = useAuthentication()
 
   const handleHideMenu = () => {
     if (menuVisible) {
       setMenuVisible(false)
     }
   }
+
   return (
-    <MenuList variant="selectedMenu" onClick={handleHideMenu}>
-      <MenuItem
-        id="home-button"
-        button="true"
-        tabIndex={0}
-        component={Link}
-        to="/Home"
-      >
-        <ListItemText>Home</ListItemText>
-      </MenuItem>
-      <MenuItem
-        id="logout-link"
-        button="true"
-        tabIndex={0}
-        component={Link}
-        to="/Logout"
-      >
-        <ListItemText>Logout</ListItemText>
-      </MenuItem>
-    </MenuList>
+    <Root>
+      <Typography variant="h6" className={classes.title}>
+        {user.firstName} {user.lastName}
+      </Typography>
+      <Typography variant="h6" className={classes.title}>
+        {user.role.roleName}
+      </Typography>
+      <br />
+      <MenuList variant="selectedMenu" onClick={handleHideMenu}>
+        <MenuItem
+          id="home-button"
+          button="true"
+          tabIndex={0}
+          component={Link}
+          to="/Home"
+        >
+          <ListItemText>Home</ListItemText>
+        </MenuItem>
+        <MenuItem
+          id="logout-link"
+          button="true"
+          tabIndex={0}
+          component={Link}
+          to="/Logout"
+        >
+          <ListItemText>Logout</ListItemText>
+        </MenuItem>
+      </MenuList>
+    </Root>
   )
 }
