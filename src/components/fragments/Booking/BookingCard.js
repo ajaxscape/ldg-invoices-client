cc6163805b27a1f1fd6bca34f3291a6c263e3e52
ben <ben@ajaxscape.com>
2022-07-24 23:04:30 +0100

    Data dependent on the logged on user

--- src/components/fragments/Booking/BookingCard.js
+++ src/components/fragments/Booking/BookingCard.js
@@ -5,7 +5,6 @@ import React, { useEffect, useState } from 'react'
 import { styled } from '@mui/material/styles'
 import { useBooking } from '../../context/BookingContext'
 import { BookingUsersTable } from './BookingUsersTable'
-import { useAuthentication } from '../../context/AuthenticationContext'
 import { CustomerCard } from '../Customer/CustomerCard'
 import { BookingDate } from './BookingDate'
 
